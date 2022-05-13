a8bc480970a5b4cc6cbd77ba3175f2d084869327
ellaxu43 <ella.xu43@gmail.com>
2022-05-13 15:41:38 -0500

    add more viz

--- visualization.py
+++ visualization.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 from wordcloud import WordCloud
+import seaborn as sns
 
 def top_languages(train):
 # creating language freq barplot (horizontal)
@@ -45,3 +46,21 @@ def word_cloud(train):
     plt.show()
 
     return
+
+def char_count(train):
+    train_subset = train[(train['language'] =='Python') | 
+                    (train['language'] =='Java')|
+                    (train['language'] =='JavaScript')|
+                    (train['language'] =='TypeScript')|
+                    (train['language'] =='C++')] 
+    plt.figure(figsize = (12, 8))
+    sns.regplot(data = train_subset, x = 'word_count', y = 'character_count',color="black",marker="+")
+    sns.scatterplot(data = train_subset, x = 'word_count', y = 'character_count',hue = 'language')
+    plt.title('Relationship Between Character Count and Word Count\n',fontsize=15)
+
+
+    plt.legend(title='Languages',title_fontsize=15,loc='center left', bbox_to_anchor=(1, 0.9))
+    plt.grid()
+    plt.ylim([0,17500])
+    plt.xlim([0,1200])
+    plt.show()

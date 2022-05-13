8ab0356a72ee0b848b21db869fe5a2de57eb39d6
Nathan Sharick <sharick.nathan@gmail.com>
2022-05-13 14:22:19 -0500

    updates

--- matt_prepare.py
+++ matt_prepare.py
@@ -19,6 +19,8 @@ from sklearn.model_selection import train_test_split
 import warnings
 warnings.filterwarnings('ignore')
 
+import numpy as np
+
 ################### ACQUIRE DATA ###################
 
 df = pd.read_json('data.json')
@@ -131,7 +133,11 @@ def clean_df(extra_words = [], exclude_words = []):
     df['lemmatized'] = tokenized_df.apply(lemmatize)
     # create columns with character and word counts
     df = df.assign(character_count= df.lemmatized.str.len(), 
-             word_count=df.lemmatized.str.split().apply(len)) 
+             word_count=df.lemmatized.str.split().apply(len))
+    #create a variable that stores a list of the top five languages
+    top_five = ['JavaScript', 'Python', 'Java', 'TypeScript', 'C++']
+    #add a column to the dataframe where any language not in the top five is represented by 'other'
+    df['top_five_languages'] = np.where(df.language.isin(top_five), df.language, 'other') 
     return df
 
 ################### SPLIT THE DATA ###################
